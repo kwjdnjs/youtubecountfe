39d7459e0abf83d4c49eb3b7371e18143b5bb277
kwjdnjs <kwjdnjs@gmail.com>
2025-02-19 12:01:25 +0900

    http

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,17 @@ import NavBar from "@/components/NavBar";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ReactNode } from "react";
 import "./globals.css";
+import Head from "next/head";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <body className="bg-gray-100">
         <NavBar />
         <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID || ""}>
