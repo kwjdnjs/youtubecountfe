e2b5555f3cd6a22f2acd5f2d99c22b49f5488fe8
kwjdnjs <kwjdnjs@gmail.com>
2025-02-19 12:12:30 +0900

    head

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        httpEquiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
+      <title>youtube view</title>
+    </>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,18 +4,10 @@ import NavBar from "@/components/NavBar";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { ReactNode } from "react";
 import "./globals.css";
-import Head from "next/head";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-        <title>youtube view</title>
-      </Head>
       <body className="bg-gray-100">
         <NavBar />
         <GoogleOAuthProvider clientId={process.env.GOOGLE_CLIENT_ID || ""}>
